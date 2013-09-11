5dc2e128070c8afcbe155542fe0af3ede2e0ed92
Nicolas Senaud <nicolas@senaud.fr>
2013-09-11 11:37:08 +0200

    valeur() & allume_led()

--- TP nº1/init_ports.c
+++ TP nº1/init_ports.c
@@ -1,8 +1,11 @@
 #include "X12.h"
 
-void init_ports (void);
-char valeur(char num_bit);
-void allume_led (char num_led, char valeur);
+void init_ports 		(void);
+char detecte_front_montant	(char num_bit);
+char valeur			(char num_bit);
+void allume_led 		(char num_led, char valeur);
+
+char cacheP = 0;
 
 void main (void)
 {
@@ -11,12 +14,13 @@ void main (void)
 
 	while(1)
 	{
-		allume_led();
+		cacheP = PTP;
 	}
 
 }
 
-void init_ports(void)
+
+void init_ports (void)
 {
 	// Initialisation des ports P et J en entrée.
 	DDRP &= ~0x30;	// ~0x30 = 0xCF.
@@ -27,61 +31,31 @@ void init_ports(void)
 	PERJ |= 0xC0;
 }
 
-void allume_led(char num_led, char valeur)
+
+char detecte_front_montant (char num_bit)
+{
+	char front = 0;
+
+	if ((PTP != cacheP) && !((PTP >> num_bit) & 0x01))
+		front = 1;
+	return front; 
+}
+
+
+char valeur (char num_bit)
+{
+	int val_lue = 0;
+
+	if (!(PTJ >> num_bit) & 0x00) val_lue = 1;
+	else val_lue = ;
+	return val_lue;
+}
+
+
+void allume_led (char num_led, char valeur)
 {
 	if (valeur)
-	switch (num_led)
-	{
-		case 0:	PORTA |= 0x01;
-			break;
-
-		case 1:	PORTA |= 0x02;
-			break;
-
-		case 2:	PORTA |= 0x04;
-			break;
-
-		case 3:	PORTA |= 0x08;
-			break;
-
-		case 4:	PORTA |= 0x10;
-			break;
-
-		case 5:	PORTA |= 0x20;
-			break;
-
-		case 6:	PORTA |= 0x40;
-			break;
-
-		case 7:	PORTA |= 0x80;
-			break;
-	}
-
+		PORTA |=  0x01 << num_led;
 	else
-	switch (num_led)
-	{
-		case 0:	PORTA &= ~0x01;
-			break;
-
-		case 1:	PORTA &= ~0x02;
-			break;
-
-		case 2:	PORTA &= ~0x04;
-			break;
-
-		case 3:	PORTA &= ~0x08;
-			break;
-
-		case 4:	PORTA &= ~0x10;
-			break;
-
-		case 5:	PORTA &= ~0x20;
-			break;
-
-		case 6:	PORTA &= ~0x40;
-			break;
-
-		case 7:	PORTA &= ~0x80;
-			break;
-	}
+		PORTA &= ~0x01 << num_led;
 }
